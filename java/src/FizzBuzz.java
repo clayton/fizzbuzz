de0cdbbcc7a74b2771d41f1ff9c84d1f29698383
Clayton <clayton@claytonlz.com>
2012-06-21 11:29:02 -0700

    Remove files that should be ignored

---
+++ java/src/FizzBuzz.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: clayton
+ * Date: 6/21/12
+ * Time: 11:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FizzBuzz {
+}
